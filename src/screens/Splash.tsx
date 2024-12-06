f41340a9efefe75be480e39f8b3c97187b54182d
suzek-klf <suzeklf@gmail.com>
2024-12-06 12:06:34 -0500

    v0.0.1 splash screen and assets

---
+++ src/screens/Splash.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import LottieView from 'lottie-react-native';
+
+export default function Splash():JSX.Element {
+    return (
+        <View>
+            <LottieView
+            
+            />
+        </View>
+    )
+}
