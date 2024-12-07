b569f79c49cf6cf75fe46a8b82a01f7951a92724
suzek-klf <suzeklf@gmail.com>
2024-12-07 16:37:09 -0500

    v0.01 lottie splash json

--- src/screens/Splash.tsx
+++ src/screens/Splash.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import LottieView from 'lottie-react-native';
 
 export default function Splash():JSX.Element {
     return (
-        <View>
             <LottieView
-            
+                style={{flex:1, backgroundColor: 'black'}}
+                source={require('../assets/lswmsSplash.json')}
+                autoPlay
+                loop={true}
+                resizeMode="cover"
             />
-        </View>
-    )
+    );
 }
